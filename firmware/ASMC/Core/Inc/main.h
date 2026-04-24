51f066381145eba1493f58b935db6268b6dd8f7e
Michał Błotniak <michalblotniak@gmail.com>
2026-04-24 19:49:41 +0200

    add cubemx config pins

---
+++ firmware/ASMC/Core/Inc/main.h
@@ -0,0 +1,93 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define STBY_CAN_1_Pin GPIO_PIN_2
+#define STBY_CAN_1_GPIO_Port GPIOC
+#define STBY_CAN_2_Pin GPIO_PIN_3
+#define STBY_CAN_2_GPIO_Port GPIOC
+#define AS_SENSE_Pin GPIO_PIN_2
+#define AS_SENSE_GPIO_Port GPIOA
+#define RES_SENSE_Pin GPIO_PIN_3
+#define RES_SENSE_GPIO_Port GPIOA
+#define RES_BYPASS_Pin GPIO_PIN_4
+#define RES_BYPASS_GPIO_Port GPIOA
+#define SPI1_NSS_Pin GPIO_PIN_8
+#define SPI1_NSS_GPIO_Port GPIOA
+#define DEBUG_LED_1_Pin GPIO_PIN_4
+#define DEBUG_LED_1_GPIO_Port GPIOB
+#define DEBUG_LED_2_Pin GPIO_PIN_5
+#define DEBUG_LED_2_GPIO_Port GPIOB
+#define DEBUG_LED_3_Pin GPIO_PIN_6
+#define DEBUG_LED_3_GPIO_Port GPIOB
+#define DEBUG_LED_4_Pin GPIO_PIN_7
+#define DEBUG_LED_4_GPIO_Port GPIOB
+#define ASSI_DI_Pin GPIO_PIN_8
+#define ASSI_DI_GPIO_Port GPIOB
+#define ASSI_DO_Pin GPIO_PIN_9
+#define ASSI_DO_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
